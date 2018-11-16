9378a6ae8d441725ac0162cba21978d9139db738
vagrant <vagrant@homestead>
2018-11-16 02:09:17 +0000

    Heroku Exec

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ URL::asset('public/css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 
 </head>
 <body>
